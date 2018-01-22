44809fe0a0f126d17101697bdf46003843cf4010
Jayden Bailey <jaydenkieran@gmail.com>
2018-01-22 03:35:23 +0000

    Wowee

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ echo "${SSH_PRIVATE_KEY}" > id_rsa
 chmod 700 id_rsa
 mkdir "${HOME}/.ssh"
 echo "${SSH_HOST_KEY}" > "${HOME}/.ssh/known_hosts"
-rsync -rcvz --delete-after --delete-excluded --rsh='ssh -p ${SSH_PORT}' _site/ ${SSH_HOSTNAME}:/srv/weirdgloop
+rsync -rcvz --delete-after --delete-excluded --rsh='ssh -p22' _site/ ${SSH_HOSTNAME}:/srv/weirdgloop
