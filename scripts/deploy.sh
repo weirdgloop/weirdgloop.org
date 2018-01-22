daa4049ec435947dd1d3f1d154d3158d4f67be30
Jayden Bailey <jaydenkieran@gmail.com>
2018-01-22 03:19:32 +0000

    Add deploy script which should totally work

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# eventually do something
-exit
+echo "${SSH_PRIVATE_KEY}" > id_rsa
+chmod 700 id_rsa
+mkdir "${HOME}/.ssh"
+echo "${SSH_HOST_KEY}" > "${HOME}/.ssh/known_hosts"
+rsync -rcvz --delete-after --delete-excluded --rsh='ssh -p ${SSH_PORT}' _site/ ${SSH_HOSTNAME}:/srv/weirdgloop
